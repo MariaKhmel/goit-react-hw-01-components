e7565c91682e167b4123c7ee1ba0b0ab3031a355
MariaKhmel <mariakhmel8@gmail.com>
2023-12-28 19:47:57 +0200

    start

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,37 @@
+// import user from '../../../src/user.json'
+
+// console.log(user);
+
+import { ProfileContainer, Avatar } from './Profile.styled';
+
+export const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes }}) => {
+    return (
+        <ProfileContainer>
+            <div class="description">
+                <Avatar
+                    src={avatar}
+                    alt="User avatar"
+                    class="avatar"
+                />
+                <p class="name">{username}</p>
+                <p class="tag">{`${tag}`}</p>
+                <p class="location">{location}</p>
+            </div>
+
+            <ul class="stats">
+                <li>
+                    <span class="label">{followers}</span>
+                    <span class="quantity">1000</span>
+                </li>
+                <li>
+                    <span class="label">{views}</span>
+                    <span class="quantity">2000</span>
+                </li>
+                <li>
+                    <span class="label">{likes}</span>
+                    <span class="quantity">3000</span>
+                </li>
+            </ul>
+        </ProfileContainer>
+    )
+}
---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ProfileContainer = styled.div`
+background-color: white;
+display: block;
+width : 100%;
+margin :  auto;
+max-width:480px;
+border-radius : 10px;
+height: 100vh;
+padding: 20px;
+`
+
+export const Avatar = styled.img`
+width: 320px;
+  height: 240px;
+  border-radius:50%;
+  margin: auto;
+
+`
